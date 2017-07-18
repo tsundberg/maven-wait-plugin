8efb492bc0e8b9061d4990ae61812883aeab1f58
Thomas Sundberg <tsu@kth.se>
2017-07-18 17:29:22 +0200

    Added a usage example

---
+++ test/src/main/java/se/thinkcode/wait/WaitServer.java
@@ -0,0 +1,26 @@
+package se.thinkcode.wait;
+
+import java.util.Set;
+
+import static spark.Spark.awaitInitialization;
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class WaitServer {
+
+    public static void main(String[] args) {
+        port(4040);
+
+        get("/geo/rest/v1/gata", (req, res) -> {
+            Set<String> headers = req.headers();
+            if (!headers.contains("applicationId")) {
+                res.status(401);
+            }
+
+            return "Hello, World!";
+        });
+
+        awaitInitialization();
+    }
+
+}
